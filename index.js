9c8cc92971bd95e3a98fd7fa6c438406645ceff4
Lydia-Tan <lydia.sydia@gmail.com>
2024-02-01 21:01:48 -0500

    changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ button.addEventListener('mouseover', function () {
     button.style.left = `${Math.ceil(Math.random() * 90)}%`;
     button.style.top = `${Math.ceil(Math.random() * 90)}%`;
 });
+
 button.addEventListener('click', function () {
     alert('y u choose no >:(')
 })
