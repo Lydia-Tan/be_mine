2cf3bf7c5d126ac7aed3f6d06afd4730745b63d6
Lydia-Tan <lydia.sydia@gmail.com>
2024-02-01 22:11:05 -0500

    Added images, starting game over and correct answer mecahnics

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const button = document.getElementById('avoid');
+const wrong_btn = document.getElementById('avoid');
+const right_btn = document.getElementById('correct_answer');
+var lives = 2
 
-button.addEventListener('mouseover', function () {
-    button.style.left = `${Math.ceil(Math.random() * 90)}%`;
-    button.style.top = `${Math.ceil(Math.random() * 90)}%`;
+wrong_btn.addEventListener('mouseover', function () {
+    wrong_btn.style.left = `${Math.floor(Math.random() * 200)}%`;
+    wrong_btn.style.top = `${Math.ceil(Math.random() * 100)}%`;
 });
 
-button.addEventListener('click', function () {
-    alert('y u choose no >:(')
-})
+// wrong_btn.addEventListener('click', function () {
+//     console.log(lives)
+//     if(lives == 0){
+//         // game over
+//         alert("GAME OVER")
+//     }
+//     else{
+//         alert('y u choose no >:(')
+//         lives -= 1;
+//     }
+// })
+
+
+// right_btn.addEventListener('click', function () {
+//     alert('nice')
+// })
+
+
