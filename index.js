d0b685bdf514ed2fd6bb2703e88c7c8ca45dcb44
Lydia-Tan <lydia.sydia@gmail.com>
2024-02-01 22:11:50 -0500

    Beginning game over mechanics

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ wrong_btn.addEventListener('mouseover', function () {
     wrong_btn.style.top = `${Math.ceil(Math.random() * 100)}%`;
 });
 
-// wrong_btn.addEventListener('click', function () {
-//     console.log(lives)
-//     if(lives == 0){
-//         // game over
-//         alert("GAME OVER")
-//     }
-//     else{
-//         alert('y u choose no >:(')
-//         lives -= 1;
-//     }
-// })
+wrong_btn.addEventListener('click', function () {
+    console.log(lives)
+    if(lives == 0){
+        // game over
+        alert("GAME OVER")
+    }
+    else{
+        alert('y u choose no >:(')
+        lives -= 1;
+    }
+})
 
 
-// right_btn.addEventListener('click', function () {
-//     alert('nice')
-// })
+right_btn.addEventListener('click', function () {
+    alert('nice')
+})
 
 
